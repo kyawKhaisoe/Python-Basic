8fa924ffcdb15e30471d20413c1dd5b202541575
kyawkhaisoe <anetworkingnard@gmail.com>
2025-11-08 14:21:40 +0630

    init commit

---
+++ chapter2.py
@@ -0,0 +1,26 @@
+my_name = 'Mg Mg'
+fav_number = 23
+height_in_feet = 5.8
+drinks_coffee = True
+
+
+print("My name is ", my_name)
+print("My fav number is", fav_number)
+print("My height is", height_in_feet)
+print("I drink coffee.(True/False)" , drinks_coffee)
+
+
+num_students = 25
+avarage_score = 88.5
+course_name = "Python Programming"
+is_course_finished = False
+
+
+#test with type() function
+
+
+print("\n-- Data Types Checking ---")
+print("num_students's Data Type:", type(num_students))
+print("avarage_score's Data Type:", type(avarage_score))
+print("course name's Data Type:", type(course_name)) 
+print("is course finished's Data Type:", type(is_course_finished) )
